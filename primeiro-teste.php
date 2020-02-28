7326fd3461eec522449d4dfcc223c2faf3eb573f
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-28 08:52:03 -0300

    Using class Conta from Conta.php

--- primeiro-teste.php
+++ primeiro-teste.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+
+require 'src/Conta.php';
+
+$conta = new Conta();
+
+$conta->nomeTitular = "Di";
+$conta->cpfTitular = "444.444.444-33";
+$conta->saldo = 500;
+
+$conta->sacar(200);
+
+echo "Saldo da conta {$conta->saldo}";
