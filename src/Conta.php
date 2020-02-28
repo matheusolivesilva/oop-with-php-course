d2f746961e8ceedd29a32be16347f390747c6686
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-28 09:00:34 -0300

    Change Conta.php, creating method sacar()

---
+++ src/Conta.php
@@ -0,0 +1,17 @@
+<?php
+                                                  class Conta
+{
+                                                          public $cpfTitular;
+        public $nomeTitular;
+        public $saldo;
+
+        public function sacar(float $valorASacar)
+        {                                                         if($valorASacar > $this->saldo) {                         echo "Saldo indisponivel!";                                                                 } else {
+                        $this->saldo -= $valorASacar;
+                }
+        }
+}
+~
+~
+~
+~
