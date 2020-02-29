28eb58a47574527d806dac57d8c14ef69df51193
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-29 11:34:42 -0300

    Refactor Conta.php, removing 'else'

--- src/Conta.php
+++ src/Conta.php
@@ -2,32 +2,39 @@
 
 class Conta
 {
-                                                          public $cpfTitular;
+       
+	public $cpfTitular;
         public $nomeTitular;
         public $saldo;
 
-        public function sacar(float $valorASacar)
-        {                                                         if($valorASacar > $this->saldo) {                         echo "Saldo indisponivel!";                                                                 } else {
-                        $this->saldo -= $valorASacar;
-                }
+        public function sacar(float $valorASacar): void
+	{                                                 
+		if($valorASacar > $this->saldo) {         
+			echo "Saldo indisponivel!";    
+			return;
+		}
+
+                $this->saldo -= $valorASacar;             
 	}
 
 	public function depositar($valorADepositar): void
 	{
 		if($valorADepositar < 0) {
  			echo "Valor precisa ser positivo";
-		} else {
-			$this->saldo += $valorADepositar;
+			return;
 		}
+
+		$this->saldo += $valorADepositar;
 	}
 
 	public function transferir(float $valorATransferir, Conta $contaDestino):void 
 	{
 		if($valorATransferir > $this->saldo) {
 			echo "Saldo insuficiente";
-		} else {
-			$this->sacar($valorATransferir);
-			$contaDestino->depositar($valorATransferir);
+			return;
 		}
+
+		$this->sacar($valorATransferir);
+		$contaDestino->depositar($valorATransferir);		
 	}
 }
