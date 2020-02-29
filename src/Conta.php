292dde351fe2f3e82fbd2b6eac5a84f864ccb189
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-28 21:07:06 -0300

    Change Conta.php, creating method 'depositar()'

--- src/Conta.php
+++ src/Conta.php
@@ -1,5 +1,6 @@
 <?php
-                                                  class Conta
+
+class Conta
 {
                                                           public $cpfTitular;
         public $nomeTitular;
@@ -9,9 +10,14 @@
         {                                                         if($valorASacar > $this->saldo) {                         echo "Saldo indisponivel!";                                                                 } else {
                         $this->saldo -= $valorASacar;
                 }
-        }
+	}
+
+	public function depositar($valorADepositar): void
+	{
+		if($valorADepositar < 0) {
+ 			echo "Valor precisa ser positivo";
+		} else {
+			$this->saldo += $valorADepositar;
+		}
+	}
 }
-~
-~
-~
-~
