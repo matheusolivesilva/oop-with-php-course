b6dc8dc637c12683c818762b0ce6f0c994e8da21
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-28 21:46:23 -0300

    Change primeiro-teste.php, using method od 'transferir()' from Conta.php

--- primeiro-teste.php
+++ primeiro-teste.php
@@ -16,3 +16,17 @@ $conta->depositar(500);
 echo PHP_EOL;
 
 echo "Saldo da conta apos deposito {$conta->saldo}";
+
+$contaTransferencia = new Conta();
+
+$contaTransferencia->nomeTitular = "Di";
+$contaTransferencia->cpfTitular = "444.444.444-33";
+$contaTransferencia->saldo = 500;
+
+$contaTransferencia->transferir(300, $conta);
+
+echo "Valor da conta 1 apos realizar  transferencia {$contaTransferencia->saldo}";
+
+
+echo "Valor da conta 2 apos realizar  transferencia {$conta->saldo}";
+
