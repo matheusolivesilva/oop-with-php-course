21525a598ec4d15cd0c7d5c952778b2dfb562b36
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-02 15:37:05 -0300

    Change Conta.php, creating methods to add and get values from the attributes

--- src/Conta.php
+++ src/Conta.php
@@ -37,4 +37,35 @@ class Conta
 		$this->sacar($valorATransferir);
 		$contaDestino->depositar($valorATransferir);		
 	}
+
+	public function recuperarSaldo(): float 
+	{
+		return $this->saldo;
+	}
+
+
+	public function recuperarCpfTitular(): string
+	{
+		return $this->cpfTitular;
+	}
+
+
+	public function recuperarNomeTitular(): string
+	{
+		return $this->nomeTitular;
+
+	}
+
+	public function defineCpfTitular(string $cpfTitular): void
+	{
+		$this->cpfTitular = $cpfTitular;
+	}
+
+
+	public function defineNomeTitular(string $nomeTitular): void
+	{
+		$this->nomeTitular = $nomeTitular;
+
+	}
+
 }
