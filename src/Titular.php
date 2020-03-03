e86e39b3910ae579d33cec955e8ad0784e85d884
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-03 02:53:21 -0300

    Change Titular.php, creating methods

--- src/Titular.php
+++ src/Titular.php
@@ -1,5 +1,28 @@
 <?php
 
 class Titular {
+	private $nome;
+	private $cpf;
 
+	public function __construct(string $cpf, string  $nome)
+	{
+		$this->validaNomeTitular($nome);
+		$this->nome = $nome;
+		$this->cpf = $cpf;
+	}
+
+	public function recuperaNome(): string
+	{
+		return $this->nome;
+	}
+
+
+	public function recuperaCpf(): string
+	{
+		return $this->cpf;
+	}
+
+	private function validaNomeTitular(string $nomeTitular)                                                     {                                                             if(strlen($nomeTitular) < 5) {
+                        echo "Nome precisa ter no minimo 5 caracteres";
+                        exit();                                       }                                             }
 }
