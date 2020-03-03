55685816d61694c260804e99ce889024be441c37
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-03 02:56:46 -0300

    Refactor banco.php, using class Titular

--- banco.php
+++ banco.php
@@ -1,20 +1,21 @@
 <?php
 
+require_once 'src/Titular.php';
 require_once 'src/Conta.php';
-$primeiraConta  = new Conta("432.383.000-66", "Joana");
+$primeiraConta  = new Conta(new Titular("432.383.000-66", "Joana"));
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
-echo "Nome: {$primeiraConta->recuperarNomeTitular()}"
-	. PHP_EOL . "CPF: {$primeiraConta->recuperarCpfTitular()}" . PHP_EOL . "Saldo: {$primeiraConta->recuperarSaldo()}";
+echo "Nome: {$primeiraConta->recuperaNome()}"
+	. PHP_EOL . "CPF: {$primeiraConta->recuperaCpf()}" . PHP_EOL . "Saldo: {$primeiraConta->recuperarSaldo()}";
 
 
-$segundaConta  = new Conta("432.383.990-66", "Maria");
+$segundaConta  = new Conta(new Titular("432.383.990-66", "Maria"));
 
 var_dump($segundaConta);
 
 unset($segundaConta);
 
-$outra = new Conta("432.883.999-66", "Daniel");
+$outra = new Conta(new Titular("432.883.999-66", "Daniel"));
 
 echo "Qtd contas: " . Conta::recuperarNumeroDeContas();
