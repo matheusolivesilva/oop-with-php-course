0d3508bb10a43b3738ff696a593d5a4ad7e44e43
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-02 22:44:34 -0300

    Refactor banco.php, changing from setters to constructor in Conta class

--- banco.php
+++ banco.php
@@ -1,12 +1,9 @@
 <?php
 
 require_once 'src/Conta.php';
-$primeiraConta  = new Conta();
+$primeiraConta  = new Conta("Matheus", "432.333.333-29");
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
-
-$primeiraConta->defineNomeTitular("Matheus");
-$primeiraConta->defineCpfTitular("432.333.333-29");
 echo "Nome: {$primeiraConta->recuperarNomeTitular()}"
 . PHP_EOL . "CPF: {$primeiraConta->recuperarCpfTitular()}" . PHP_EOL . "Saldo: {$primeiraConta->recuperarSaldo()}";
