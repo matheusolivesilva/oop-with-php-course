7d8c37370bd51487537e9058de62e88a3ef71da6
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-03 01:45:28 -0300

    Change banco.php, using static function

--- banco.php
+++ banco.php
@@ -1,9 +1,18 @@
 <?php
 
 require_once 'src/Conta.php';
-$primeiraConta  = new Conta("432.383.000-66", "Joao");
+$primeiraConta  = new Conta("432.383.000-66", "Joana");
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
 echo "Nome: {$primeiraConta->recuperarNomeTitular()}"
-. PHP_EOL . "CPF: {$primeiraConta->recuperarCpfTitular()}" . PHP_EOL . "Saldo: {$primeiraConta->recuperarSaldo()}";
+	. PHP_EOL . "CPF: {$primeiraConta->recuperarCpfTitular()}" . PHP_EOL . "Saldo: {$primeiraConta->recuperarSaldo()}";
+
+
+$segundaConta  = new Conta("432.383.990-66", "Maria");
+
+var_dump($segundaConta);
+
+new Conta("432.883.999-66", "Daniel");
+
+echo "Qtd contas: " . Conta::recuperarNumeroDeContas();
