cdfe9e102b0a731d60ec3fc336d0e3c742b634df
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-09 09:23:12 -0300

    Create teste-saque.php, using 'sacar()' method from Conta class

---
+++ teste-saque.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'autoload.php';
+
+use Alura\Banco\Model\Conta\Titular;
+use Alura\Banco\Model\Conta\Conta;
+use Alura\Banco\Model\CPF;
+use Alura\Banco\Model\Endereco;
+
+$conta = new Conta(
+		new Titular(
+			new CPF('888.888.999-99'),
+			'Dididi',
+			new Endereco('São Paulo', 'Teste', 'Rua teste', '888')
+		)
+);
+$conta->depositar(600);
+
+$conta->sacar(100);
+
+echo $conta->recuperarSaldo();
+
