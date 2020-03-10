3b70f591137467ad4cc706f3c08088a350386103
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-10 19:38:31 -0300

    Change Funcionario.php

--- src/Model/Funcionario.php
+++ src/Model/Funcionario.php
@@ -3,14 +3,25 @@ namespace Alura\Banco\Model;
 class Funcionario extends Pessoa
 {
 	private $cargo;
+	private $salario;
 
-	public function __construct(string $nome, CPF $cpf, string $cargo) {
+	public function __construct(string $nome, CPF $cpf, string $cargo, float $salario) {
 		parent::__construct($nome, $cpf);
 		$this->cargo = $cargo;
+		$this->salario = $salario;
 	}
 	
 	public function recuperaCargo(): string
 	{
 		return $this->cargo;
+	}
+	
+	public function recuperaSalario(): float
+	{
+		return $this->salario;
+	}
+	public function calculaBonificacao(): float
+	{
+		return $this->salario * 0.1;;
 	}
 }
